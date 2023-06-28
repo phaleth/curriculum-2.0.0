5bc1ca1ec7d4db5617efae6b71399b9cbfbe72ff
phaleth <29838606+phaleth@users.noreply.github.com>
2023-06-28 23:51:25 +0200

    revisit refactor of GildedRose-Refactoring-Kata

--- projects/GildedRose-Refactoring-Kata/test/gilded_rose_test.exs
+++ projects/GildedRose-Refactoring-Kata/test/gilded_rose_test.exs
@@ -8,7 +8,64 @@ defmodule GildedRoseTest do
     assert "foo" === first_item_name
   end
 
-  test "example code test" do
+  test "Once the sell by date has passed, Quality degrades twice as fast" do
+    updated_items =
+      GildedRose.update_quality([
+        %Item{name: "normal", sell_in: 9, quality: 1},
+        %Item{name: "normal", sell_in: 0, quality: 1},
+        %Item{name: "normal", sell_in: -1, quality: 1}
+      ])
+
+    assert Enum.all?(updated_items, &(&1.quality === 0))
+  end
+
+  test "The Quality of an item is never negative" do
+    updated_items =
+      GildedRose.update_quality([
+        %Item{name: "normal", sell_in: 0, quality: 2},
+        %Item{name: "normal", sell_in: -1, quality: 2}
+      ])
+
+    assert Enum.all?(updated_items, &(&1.quality === 0))
+  end
+
+  test "\"Aged Brie\" actually increases in Quality the older it gets" do
+    updated_items =
+      GildedRose.update_quality([
+        %Item{name: "Aged Brie", sell_in: 0, quality: 2},
+        %Item{name: "Aged Brie", sell_in: -1, quality: 2}
+      ])
+
+    assert Enum.all?(updated_items, &(&1.quality === 4))
+  end
+
+  test "The Quality of an item is never more than 50" do
+    updated_items =
+      GildedRose.update_quality([
+        %Item{name: "Aged Brie", sell_in: 9, quality: 50},
+        %Item{name: "Aged Brie", sell_in: 0, quality: 50},
+        %Item{name: "Aged Brie", sell_in: -1, quality: 50}
+      ])
+
+    assert Enum.all?(updated_items, &(&1.quality === 50))
+  end
+
+  @tag :skip
+  test "\"Sulfuras\", being a legendary item, never has to be sold or decreases in Quality" do
+    updated_items =
+      GildedRose.update_quality([
+        %Item{name: "Sulfuras", sell_in: 9, quality: 80},
+        %Item{name: "Sulfuras", sell_in: 0, quality: 80},
+        %Item{name: "Sulfuras", sell_in: -1, quality: 80}
+      ])
+
+    assert Enum.all?(updated_items, &(&1.quality === 80))
+  end
+
+  @tag :skip
+  test "\"Backstage passes\", like aged brie, increases in Quality as its SellIn value approaches"
+
+  test "Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less" do
     items =
       GildedRose.update_quality([
         %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 9, quality: 1}
@@ -20,4 +77,7 @@ defmodule GildedRoseTest do
 
     assert items === expected
   end
+
+  @tag :skip
+  test "Quality drops to 0 after the concert"
 end
