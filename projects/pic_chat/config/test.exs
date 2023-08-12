9a6d8fb44be66b7fd8be784b7945849c1b56f409
phaleth <29838606+phaleth@users.noreply.github.com>
2023-08-12 17:14:19 +0200

    PicChat: Setup test environment

--- projects/pic_chat/config/test.exs
+++ projects/pic_chat/config/test.exs
@@ -6,9 +6,10 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :pic_chat, PicChat.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
+  username: System.get_env("POSTGRES_USERNAME") || "postgres",
+  password: System.get_env("POSTGRES_PASSWORD") || "postgres",
+  hostname: System.get_env("POSTGRES_HOSTNAME") || "localhost",
+  port: System.get_env("POSTGRES_PORT") || 5432,
   database: "pic_chat_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
